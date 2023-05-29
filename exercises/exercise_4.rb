5d38bac7972bb797e251e58503d905e005e532c8
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-29 15:29:02 -0700

    ex 4 done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@womens = Store.where(womens_apparel: true)
+.where('annual_revenue < ?', 1000000)
+@mens = Store.where(mens_apparel: true)
+
+puts @womens
+puts @mens
